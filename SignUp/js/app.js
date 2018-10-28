9d3fd288b9241d475927f281e4ebc68ffacfc075
M Yousuf Soomro <muhammad.yousuf1996@gmail.com>
2018-10-29 00:34:37 +0500

    updated

--- SignUp/js/app.js
+++ SignUp/js/app.js
@@ -1,126 +1,83 @@
-var temp1 = false;
-var temp2 = false;
-var temp3 = false;
-var temp4 = false;
-// var temp5 = false;
-var fNameArr = [];
-var lNameArr = [];
-var emailArr = [];
-var passArr = [];
-var genderArr = [];
-var userDetails = [fNameArr, lNameArr, emailArr, passArr];
-function adminInfo() {
-    localStorage.setItem('adminEmail', 'admin@example.com');
-    localStorage.setItem('adminPassword', '123456789');
-}
-function signUp() {
-    validateFirstName();
-    validateLastName();
-    validateEmail();
-    validatePassword();
-    // validateGender();
-    localStorage.setItem('userDetails', userDetails)
-    if (temp1 && temp2 && temp3 && temp4) {
-        window.location.href = "login.html"
-    } else {
-        // window.location.href = window.location.href;
-    }
-}
-function validateFirstName() {
+function RunTest() {
     var fName = document.getElementById('fName').value;
-    if (fName !== "" && fName !== " ") {
-        fNameArr.push(fName);
-        console.log(fNameArr);
-        localStorage.setItem('FirstName', fName);
-        console.log("right");
-        temp1 = true;
-    } else {
-        alert("Please Enter correct Correct First Name");
-        console.log("wrong");
-        temp1 = false;
-    }
-}
-
-function validateLastName() {
     var lName = document.getElementById('lName').value;
-    if (lName !== "" && lName !== " ") {
-        lNameArr.push(lName);
-        localStorage.setItem('LastName', lName);
-        console.log("right");
-        temp2 = true;
-    } else {
-        alert("Please Enter correct Correct Last Name");
-        console.log("wrong");
-        temp2 = false;
-    }
-}
-
-function validateEmail() {
     var email = document.getElementById('email').value;
-    if (email.indexOf(" ") !== -1) {
-        alert("Space are not allowed in Email\nPlease Try again!");
-    } else if (email.indexOf("@") <= 1 && email.indexOf("@") < email.length - 5) {
-        alert("Please enter a valid Email Address\nPlease Try again!");
-    } else if (email.indexOf(".") === -1) {
-        alert("You have not entered . (dot) in Email Address");
-    } else {
-        emailArr.push(email);
-        localStorage.setItem('email', email);
-        temp3 = true;
-    }
-}
-
-function validatePassword() {
     var password = document.getElementById('password').value;
-    var confPass = document.getElementById('confPass').value;
-    if (password.indexOf(" ") !== -1) {
-        alert("Space are not allowed in Password\nPlease Try again!");
-    } else if(password !== confPass){
-        alert("Please enter the same Password\nPlease Try again!");
-    }else {
-        passArr.push(password);
-        localStorage.setItem('password', password);
-        temp4 = true;
-    }
-}
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var termsCond = document.getElementById('customCheck1');
 
-function login() {
-    var email = localStorage.getItem('email');
-    var adminEmail = localStorage.getItem('adminEmail');
-    var password = localStorage.getItem('password');
-    var adminPassword = localStorage.getItem('adminPassword');
-    var userEmail = document.getElementById('userEmail').value;
-    var userPassword = document.getElementById('userPassword').value;
+    var NameReg = /^([a-zA-Z])/;
+    var EmailReg = /([a-z\d_-]+)@([a-z\d_-]+)\.[a-z]{2,4}/;
+    var PasswordReg = /^([a-zA-Z0-9]{6,10})/;
 
-    if (email === userEmail && password === userPassword) {
-        window.location.href = 'welcome.html'
-        setTimeout(userInfo, 3000)
-        document.getElementById('headOne').innerHTML = "Welcome " + localStorage.getItem('FirstName') + " " + localStorage.getItem('LastName');
-    } else if (userEmail === adminEmail && userPassword === adminPassword) {
-        window.location.href = 'adminPage.html'
+    var dataObj = {};
+
+    if (fName.match(NameReg)) {
+        document.getElementById("fNameError").innerText = "";
+        dataObj.fName = fName;
     } else {
-        alert('Your Email / Password not correct\nTry Again!');
+        document.getElementById("fNameError").innerText = "Please Enter First Name";
+        document.getElementById('fNameError').setAttribute('class', "text-danger");
+        return false;
+    }
+
+    if (lName.match(NameReg)) {
+        document.getElementById("lNameError").innerText = "";
+        dataObj.lName = lName;
+    } else {
+        document.getElementById("lNameError").innerText = "Please Enter Last Name";
+        document.getElementById('lNameError').setAttribute('class', "text-danger");
+        return false;
+    }
+
+    if (email.match(EmailReg)) {
+        document.getElementById("emailError").innerText = "";
+        dataObj.email = email;
+    } else {
+        document.getElementById("emailError").innerText = "Please Correct Email Address";
+        document.getElementById('emailError').setAttribute('class', "text-danger");
+        return false;
+    }
+
+    if (password.match(PasswordReg)) {
+        document.getElementById("passwordError").innerText = "";
+        if (password === confirmPassword) {
+            document.getElementById("confirmPasswordError").innerText = "";
+            dataObj.password = password;
+        } else {
+            document.getElementById("confirmPasswordError").innerText = "Please Enter Same Password";
+            document.getElementById('confirmPasswordError').setAttribute('class', "text-danger");
+            return false;
+        }
+    } else {
+        document.getElementById("passwordError").innerText = "Password Must be Between 6 - 10 contains A-Z, a-z & 0-9 only";
+        document.getElementById('passwordError').setAttribute('class', "text-danger");
+        return false;
+    }
+
+    if (termsCond.checked){
+        document.getElementById("termsCondError").innerText = "";
+        console.log(dataObj);
+        localStorage.setItem('userInfo', JSON.stringify(dataObj));
+        resetForm();
+        swal({
+            title: "Account Created!",
+            text: "Your account has been successfuly created!",
+            icon: "success",
+            button: "OK!",
+          });
+    }else{
+        document.getElementById("termsCondError").innerText = "You must agree before submitting.";
+        document.getElementById('termsCondError').setAttribute('class', "text-danger");
+        return false;
     }
 }
 
-// function gender() {
-//     var gender = document.getElementsByName('gender');
-//     console.log("TEST GENDER:>>> " + gender);
-//     if (gender === "male" || gender === "female" || gender === "other") {
-//         return gender;
-//     }
-//     alert('Please Select your gender');
-//     return false;
-// }
-
-// function validateGender() {
-//     var getGender = gender();
-//     console.log(getGender);
-//     if (getGender !== false) {
-//         genderArr.push(getGender);
-//         localStorage.setItem('gender', getGender);
-//         temp5 = true;
-//     } else {
-//         temp5 = false;
-//     }
-// }
+function resetForm(){
+    document.getElementById('fName').value = "";
+    document.getElementById('lName').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('password').value = "";
+    document.getElementById('confirmPassword').value = "";
+    document.getElementById('customCheck1').removeAttribute('checked');
+}
